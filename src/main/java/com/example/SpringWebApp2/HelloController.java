95e6215bafd791705e624770be83b6688c15fc4b
jayeshjaiswal <jayeshjaiswal.developer@gmail.com>
2026-01-07 14:24:08 +0530

    firstCommit

---
+++ src/main/java/com/example/SpringWebApp2/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebApp2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot + Docker + Jenkins";
+    }
+}
---
+++ src/test/java/com/example/SpringWebApp2/SpringWebApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringWebApp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
